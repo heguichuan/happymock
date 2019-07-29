428977ae4929f0bab12f10f8b742fa844b43feb6
heguichuan <heguichuan@cmiot.chinamobile.com>
2019-07-29 19:22:42 +0800

    写文件

---
+++ template/db-tpl.js
@@ -0,0 +1,25 @@
+var Mock = require("mockjs");
+
+module.exports = {
+    test1: Mock.mock({
+        "error": 1,
+        "message": "success3",
+        "result|40": [
+            {
+                "author": "@name",
+                "comment": "@cparagraph",
+                "date": "@datetime"
+            }
+        ]
+    }),
+    success: {
+        code: 0,
+        msg: "succ",
+        data: null
+    },
+    fail: {
+        code: 500,
+        msg: "接口错误咯！",
+        data: null
+    }
+};
---
+++ template/route-tpl.js
@@ -0,0 +1,6 @@
+// 路由
+module.exports = {
+    "/pathabc/test1": "/test1",
+    "/a/b/c/getDatas*": "/success",
+    "/fail*": "/fail"
+};

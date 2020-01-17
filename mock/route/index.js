919eb30590030b6a8df9f24c78e15ab7ff8965ff
heguichuan <heguichuan@cmiot.chinamobile.com>
2020-01-17 14:18:59 +0800

    chore: 修改package.json

--- mock/route/index.js
+++
@@ -1,6 +0,0 @@
-// routes.js
-module.exports = {
-    "/Firmware/GetUpgradeBagList*": "/GetUpgradeBagList",
-    "/Firmware/GetUserProductList*": "/GetUserProductList",
-    "/Comm/GetOtaModule*": "/GetOtaModule"
-};
